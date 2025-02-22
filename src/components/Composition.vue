a7e725fff2d2e2046aa8ee987e6c9bb4e88e098b
Bala Logesh S <balassmp@gmail.com>
2025-02-21 23:12:36 -0500

    Added base html and css

--- src/components/Composition.vue
+++ src/components/Composition.vue
@@ -1,13 +1,28 @@
 <template>
-    <h1>Composition API</h1>
+    <div class="container mt-4 text-center" style="max-width: 600px;">
+        <h1 class="mb-5">You have x Todos</h1>
+        <div class="input-group mb-5">
+            <input type="text" class="form-control p-3" placeholder="Add a Todo">
+            <button class="btn btn-success" style="width: 125px;">Add</button>
+        </div>
+        <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                One <span class="badge bg-danger">x</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                Two <span class="badge bg-danger">x</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                Three <span class="badge bg-danger">x</span>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Options.vue
+++ src/components/Options.vue
@@ -1,5 +1,22 @@
 <template>
-    <h1>Options API</h1>
+    <div class="container mt-4 text-center" style="max-width: 600px;">
+        <h1 class="mb-5">You have x Todos</h1>
+        <div class="input-group mb-5">
+            <input type="text" class="form-control p-3" placeholder="Add a Todo">
+            <button class="btn btn-primary" style="width: 125px;">Add</button>
+        </div>
+        <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                One <span class="badge bg-danger">x</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                Two <span class="badge bg-danger">x</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                Three <span class="badge bg-danger">x</span>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
