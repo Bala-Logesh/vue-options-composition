197363b47da6e1f57d920b115b1eae8eb9ad6dd2
Bala Logesh S <balassmp@gmail.com>
2025-02-22 00:00:04 -0500

    Completed composition API

--- src/components/Composition.vue
+++ src/components/Composition.vue
@@ -1,27 +1,75 @@
 <template>
     <div class="container mt-4 text-center" style="max-width: 600px;">
-        <h1 class="mb-5">You have x Todos</h1>
+        <h1 class="mb-5">You have {{ todosCount }} Todos</h1>
         <div class="input-group mb-5">
-            <input type="text" class="form-control p-3" placeholder="Add a Todo">
-            <button class="btn btn-success" style="width: 125px;">Add</button>
+            <input type="text" class="form-control p-3" placeholder="Add a Todo" v-model="newTodoName"
+                @keydown.enter="addTodo">
+            <button class="btn btn-primary" style="width: 125px;" @click="addTodo">Add</button>
         </div>
         <ul class="list-group">
-            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                One <span class="badge bg-danger">x</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                Two <span class="badge bg-danger">x</span>
-            </li>
-            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                Three <span class="badge bg-danger">x</span>
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3" v-for="todo in todos"
+                :key="todo.id">
+                {{ todo.name }} <span class="badge bg-danger pointer" @click="deleteTodo(todo.id)"
+                    role="button">x</span>
             </li>
         </ul>
     </div>
 </template>
 
 <script>
-export default {
+import { ref, computed, watch } from 'vue'
 
+export default {
+    setup() {
+        let newTodoName = ref('')
+        let todos = ref([
+            {
+                id: 1,
+                name: 'One'
+            },
+            {
+                id: 2,
+                name: 'Two'
+            },
+            {
+                id: 3,
+                name: 'Three'
+            },
+        ])
+
+        const excludedWords = ['fail', 'die', 'quit']
+
+        let todosCount = computed(() => {
+            return todos.value.length
+        })
+
+        function addTodo() {
+            if (newTodoName.value == "") return
+
+            todos.value.push({ id: Date.now(), name: newTodoName.value })
+            newTodoName.value = ''
+        }
+
+        const deleteTodo = id => {
+            todos.value = todos.value.filter(todo => todo.id !== id)
+        }
+
+        watch(newTodoName, (newValue) => {
+            if (excludedWords.includes(newValue.toLowerCase())) {
+                newTodoName.value = ''
+                alert('Never use the word ' + newValue + "!!!!")
+            }
+        })
+
+        return {
+            newTodoName,
+            todos,
+            excludedWords,
+            todosCount,
+            addTodo,
+            deleteTodo
+        }
+    }
 }
 </script>
 
