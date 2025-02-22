5ea800a0cadb482cb08d8c63af370cffc5007716
Bala Logesh S <balassmp@gmail.com>
2025-02-22 00:06:08 -0500

    Switched to reactive objs instead of refs

--- src/components/Composition.vue
+++ src/components/Composition.vue
@@ -2,12 +2,12 @@
     <div class="container mt-4 text-center" style="max-width: 600px;">
         <h1 class="mb-5">You have {{ todosCount }} Todos</h1>
         <div class="input-group mb-5">
-            <input type="text" class="form-control p-3" placeholder="Add a Todo" v-model="newTodoName"
+            <input type="text" class="form-control p-3" placeholder="Add a Todo" v-model="data.newTodoName"
                 @keydown.enter="addTodo">
             <button class="btn btn-primary" style="width: 125px;" @click="addTodo">Add</button>
         </div>
         <ul class="list-group">
-            <li class="list-group-item d-flex justify-content-between align-items-center p-3" v-for="todo in todos"
+            <li class="list-group-item d-flex justify-content-between align-items-center p-3" v-for="todo in data.todos"
                 :key="todo.id">
                 {{ todo.name }} <span class="badge bg-danger pointer" @click="deleteTodo(todo.id)"
                     role="button">x</span>
@@ -17,53 +17,54 @@
 </template>
 
 <script>
-import { ref, computed, watch } from 'vue'
+import { computed, watch, reactive } from 'vue'
 
 export default {
     setup() {
-        let newTodoName = ref('')
-        let todos = ref([
-            {
-                id: 1,
-                name: 'One'
-            },
-            {
-                id: 2,
-                name: 'Two'
-            },
-            {
-                id: 3,
-                name: 'Three'
-            },
-        ])
+        const data = reactive({
+            newTodoName: '',
+            todos: [
+                {
+                    id: 1,
+                    name: 'One'
+                },
+                {
+                    id: 2,
+                    name: 'Two'
+                },
+                {
+                    id: 3,
+                    name: 'Three'
+                },
+            ]
+        })
 
         const excludedWords = ['fail', 'die', 'quit']
 
         let todosCount = computed(() => {
-            return todos.value.length
+            return data.todos.length
         })
 
         function addTodo() {
-            if (newTodoName.value == "") return
+            if (data.newTodoName == "") return
 
-            todos.value.push({ id: Date.now(), name: newTodoName.value })
-            newTodoName.value = ''
+            data.todos.push({ id: Date.now(), name: data.newTodoName })
+            data.newTodoName = ''
         }
 
         const deleteTodo = id => {
-            todos.value = todos.value.filter(todo => todo.id !== id)
+            data.todos = data.todos.filter(todo => todo.id !== id)
         }
 
-        watch(newTodoName, (newValue) => {
-            if (excludedWords.includes(newValue.toLowerCase())) {
-                newTodoName.value = ''
-                alert('Never use the word ' + newValue + "!!!!")
+        watch(data, (newValue) => {
+            if (excludedWords.includes(newValue.newTodoName.toLowerCase())) {
+                alert('Never use the word ' + newValue.newTodoName + "!!!!")
+                data.newTodoName = ''
             }
         })
 
         return {
-            newTodoName,
-            todos,
+            data,
             excludedWords,
             todosCount,
             addTodo,
